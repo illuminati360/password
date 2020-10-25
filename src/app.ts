dacdf9bfc06077b37269b397830fb5dfcc2cd2cf
illuminati360 <johnsonyuehit@gmail.com>
2020-10-25 19:53:21 +0800

    Added override

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,9 @@ const BLOCKER_OFFSET_X = 2.8;
 const BLOCKER_OFFSET_Y = 0;
 const BLOCKER_OFFSET_Z = BLOCKER_DEPTH/2;
 
+const OVERRIDE_WIDTH = 0.2;
+const OVERRIDE_HEIGHT = 0.2;
+const OVERRIDE_DEPTH = 0.005;
 const OVERRIDE_OFFSET_X = 3.2;
 const OVERRIDE_OFFSET_Y = 1;
 const OVERRIDE_OFFSET_Z = -BLOCKER_DEPTH/2;
@@ -311,6 +314,7 @@ export default class Numlock {
             this.grantAccess();
             this.override.updateColor(MRE.Color3.Green());
         }
+        this.isGranted = !this.isGranted;
     }
 
     private playSound(musicAsset: MRE.Sound){
